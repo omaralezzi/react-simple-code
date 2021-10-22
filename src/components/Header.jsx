78a30ea8dfbddde5682cd8f840bd42d031eb79d7
omaralezzi <81753019+omaralezzi@users.noreply.github.com>
2021-10-22 13:23:50 +0200

    add more function as a test add more tes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,18 @@ import React from "react";
 
 const Header = ({ title, name }) => {
   // console.log("my props:",props.title);
+  // function bothString () { return `${title} ${name}`};
+  const bothString = () => {
+    return `${title} ${name}`;
+  };
+
+  // console.log(bothString ());
   return (
     <header>
       <h1>React task</h1>
       <h3>{title}</h3>
       <h3>{name}</h3>
+      <h3>{bothString()}</h3>
     </header>
   );
 };
